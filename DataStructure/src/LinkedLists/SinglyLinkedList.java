3144841b31e22c960b98d2037af46e9be2e03738
MAN <MAN@MAN-PC>
2017-12-02 17:17:32 +0900

    equals, clone() complete

--- DataStructure/src/LinkedLists/SinglyLinkedList.java
+++ DataStructure/src/LinkedLists/SinglyLinkedList.java
@@ -1,6 +1,6 @@
 package LinkedLists;
 
-public class SinglyLinkedList<E> {
+public class SinglyLinkedList<E> implements Cloneable{
 	
 	private static class Node<E>{
 		private E element;
@@ -69,5 +69,36 @@ public class SinglyLinkedList<E> {
 		size--;
 		if(size==0) tail=null;
 		return answer;
+	}
+	
+	public boolean equals(Object o) {
+		if(o==null) return false;
+		if(getClass()!=o.getClass()) return false;
+		SinglyLinkedList<E> other = (SinglyLinkedList<E>) o;
+		if(size!=other.size) return false;
+		Node walkA = head;
+		Node walkB = other.head;
+		while(walkA!=null) {
+			if(!walkA.getElement().equals(walkB.getElement())) return false;
+			walkA=walkA.getNext();
+			walkB=walkB.getNext();
+		}
+		return true;
+	}
+	
+	public SinglyLinkedList<E> clone() throws CloneNotSupportedException{
+		SinglyLinkedList<E> other = (SinglyLinkedList<E>) super.clone();
+		if(size>0) {
+			other.head = new Node<E>(head.getElement(),null);
+			Node<E> walk = head.getNext();
+			Node<E> otherTail = other.head;
+			while(walk!=null) {
+				Node<E> newest = new Node<E>(walk.getElement(),null);
+				otherTail.setNext(newest);
+				otherTail=newest;
+				walk=walk.getNext();
+			}
+		}
+		return other;
 	}
 }
